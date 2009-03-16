cc8a84cbd00970b7a531aaa4fdc7bd1a43226cb4
Tekkub <tekkub@gmail.com>
2009-03-15 19:10:16 -0600

    Update filter for 3.1

--- Quelevel.lua
+++ Quelevel.lua
@@ -66,7 +66,12 @@ end)
 
 
 -- Add tags to quest links in chat
-local function filter(msg) if msg then return false, msg:gsub("(|c%x+|Hquest:%d+:(%d+))", "(%2) %1") end end
+local function filter(self, event, msg, ...)
+	if select(4, GetBuildInfo()) < 30100 then msg = self end
+	if msg then
+		return false, msg:gsub("(|c%x+|Hquest:%d+:(%d+))", "(%2) %1"), ...
+	end
+end
 for _,event in pairs{"SAY", "GUILD", "GUILD_OFFICER", "WHISPER", "WHISPER_INFORM", "PARTY", "RAID", "RAID_LEADER", "BATTLEGROUND", "BATTLEGROUND_LEADER"} do ChatFrame_AddMessageEventFilter("CHAT_MSG_"..event, filter) end
 
 
